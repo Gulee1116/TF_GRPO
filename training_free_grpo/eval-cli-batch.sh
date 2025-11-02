bb43eef51ab374f73e40bcbe17b152b2982e4afc
YongliangMiao <123933719+YongliangMiao@users.noreply.github.com>
2025-11-02 23:16:36 +0800

    Add files via upload
    测试mean@8，pass@8

--- training_free_grpo/eval-cli-batch.sh
+++ training_free_grpo/eval-cli-batch.sh
@@ -1,22 +1,7 @@
 # AIME24
-    # Mean@32
-    for i in {1..32}; do
-        bash training_free_grpo/eval-cli.sh --dataset AIME24 --pass_k 1 --experience_exist y
-        bash training_free_grpo/eval-cli.sh --dataset AIME24 --pass_k 1 --experience_exist n
-    done
+bash training_free_grpo/eval-cli.sh --dataset AIME24 --pass_k 8 --experience_exist y
+bash training_free_grpo/eval-cli.sh --dataset AIME24 --pass_k 8 --experience_exist n
 
 # AIME25
-    # Mean@32
-    for i in {1..32}; do
-        bash training_free_grpo/eval-cli.sh --dataset AIME25 --pass_k 1 --experience_exist y
-        bash training_free_grpo/eval-cli.sh --dataset AIME25 --pass_k 1 --experience_exist n
-    done
-
-    # Pass@8
-    bash training_free_grpo/eval-cli.sh --dataset AIME25 --pass_k 8 --experience_exist y 
-    bash training_free_grpo/eval-cli.sh --dataset AIME25 --pass_k 8 --experience_exist n
-
-    # Pass@32
-    bash training_free_grpo/eval-cli.sh --dataset AIME25 --pass_k 32 --experience_exist y
-    bash training_free_grpo/eval-cli.sh --dataset AIME25 --pass_k 32 --experience_exist n
-         
+bash training_free_grpo/eval-cli.sh --dataset AIME25 --pass_k 8 --experience_exist y
+bash training_free_grpo/eval-cli.sh --dataset AIME25 --pass_k 8 --experience_exist n
