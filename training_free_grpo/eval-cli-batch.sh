91435983e0b3481908aefd51117fc21fb13edf24
wziyue <2094650552@qq.com>
2025-11-10 16:49:35 +0800

    modify eval-cli-batch

--- training_free_grpo/eval-cli-batch.sh
+++ training_free_grpo/eval-cli-batch.sh
@@ -1,10 +1,14 @@
 # AIME24
-# bash training_free_grpo/eval-cli.sh --dataset AIME24 --pass_k 8 --experience_exist y
+bash training_free_grpo/eval-cli.sh --dataset AIME24 --pass_k 8 --experience_exist y
 bash training_free_grpo/eval-cli.sh --dataset AIME24 --pass_k 8 --experience_exist n
 
 # AIME25
 bash training_free_grpo/eval-cli.sh --dataset AIME25 --pass_k 8 --experience_exist y
 bash training_free_grpo/eval-cli.sh --dataset AIME25 --pass_k 8 --experience_exist n
+
+
+
+
     # for i in {2..32}; do
     #     bash training_free_grpo/eval-cli.sh --dataset AIME24 --pass_k 1 --experience_exist n
     #     echo "done doing ${i}/32 eval AIME24 EXP N"
